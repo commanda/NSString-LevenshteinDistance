2e7db505f7da557daa90d889ed19b566c65aac08
Amanda Wixted <amanda@amandawixted.com>
2016-06-02 11:08:36 -0500

    Initial Commit

---
+++ StringScoreProject/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  StringScoreProject
+//
+//  Created by Amanda Wixted on 6/2/16.
+//  Copyright © 2016 Amanda Wixted. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
