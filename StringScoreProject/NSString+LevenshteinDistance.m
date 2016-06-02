d0250576020a9cee6a6c7db90e2b693e99ddf60c
Amanda Wixted <amanda@amandawixted.com>
2016-06-02 11:54:10 -0500

    renamed some vars

--- StringScoreProject/NSString+LevenshteinDistance.m
+++ StringScoreProject/NSString+LevenshteinDistance.m
@@ -22,23 +22,23 @@
     // Step 1 (Steps follow description at http://www.merriampark.com/ld.htm)
     NSInteger k, i, j, cost, * d, distance;
     
-    NSInteger n = [originalString length];
-    NSInteger m = [comparisonString length];
+    NSInteger lenOriginal = [originalString length];
+    NSInteger lenComparison = [comparisonString length];
     
-    if( n++ != 0 && m++ != 0 ) {
+    if( lenOriginal++ != 0 && lenComparison++ != 0 ) {
         
-        d = malloc( sizeof(NSInteger) * m * n );
+        d = malloc( sizeof(NSInteger) * lenComparison * lenOriginal );
         
         // Step 2
-        for( k = 0; k < n; k++)
+        for( k = 0; k < lenOriginal; k++)
             d[k] = k;
         
-        for( k = 0; k < m; k++)
-            d[ k * n ] = k;
+        for( k = 0; k < lenComparison; k++)
+            d[ k * lenOriginal ] = k;
         
         // Step 3 and 4
-        for( i = 1; i < n; i++ )
-            for( j = 1; j < m; j++ ) {
+        for( i = 1; i < lenOriginal; i++ )
+            for( j = 1; j < lenComparison; j++ ) {
                 
                 // Step 5
                 if( [originalString characterAtIndex: i-1] ==
@@ -48,7 +48,7 @@
                     cost = 1;
                 
                 // Step 6
-                d[ j * n + i ] = MIN(MIN(d [ (j - 1) * n + i ] + 1, d[ j * n + i - 1 ] +  1), d[ (j - 1) * n + i - 1 ] + cost);
+                d[ j * lenOriginal + i ] = MIN(MIN(d [ (j - 1) * lenOriginal + i ] + 1, d[ j * lenOriginal + i - 1 ] +  1), d[ (j - 1) * lenOriginal + i - 1 ] + cost);
                 
                 // This conditional adds Damerau transposition to Levenshtein distance
                 if( i>1 && j>1 && [originalString characterAtIndex: i-1] ==
@@ -56,11 +56,11 @@
                    [originalString characterAtIndex: i-2] ==
                    [comparisonString characterAtIndex: j-1] )
                 {
-                    d[ j * n + i] = MIN( d[ j * n + i ], d[ (j - 2) * n + i - 2 ] + cost);
+                    d[ j * lenOriginal + i] = MIN( d[ j * lenOriginal + i ], d[ (j - 2) * lenOriginal + i - 2 ] + cost);
                 }
             }
         
-        distance = d[ n * m - 1 ];
+        distance = d[ lenOriginal * lenComparison - 1 ];
         
         free( d );
         
